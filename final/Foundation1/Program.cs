b775c76b36d4770060bd7d707bf5e8a5281eea91
Yohannsokora <156164442+Yohannsokora@users.noreply.github.com>
2024-04-02 00:05:51 -0600

    final project week 12 submission

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,70 @@
 using System;
+using System.Collections.Generic;
+
+class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Length { get; set; } // Length in seconds
+    public List<Comment> Comments { get; set; }
+
+    public Video(string title, string author, int length)
+    {
+        Title = title;
+        Author = author;
+        Length = length;
+        Comments = new List<Comment>();
+    }
+
+    public int GetNumberOfComments()
+    {
+        return Comments.Count;
+    }
+}
+
+class Comment
+{
+    public string CommenterName { get; set; }
+    public string Text { get; set; }
+
+    public Comment(string commenterName, string text)
+    {
+        CommenterName = commenterName;
+        Text = text;
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Create videos
+        List<Video> videos = new List<Video>
+        {
+            new Video("Video 1", "Author 1", 120),
+            new Video("Video 2", "Author 2", 180),
+            new Video("Video 3", "Author 3", 150)
+        };
+
+        // Add comments
+        videos[0].Comments.Add(new Comment("User 1", "Comment 1 for First Video "));
+        videos[0].Comments.Add(new Comment("User 2", "Comment 2 for First Video "));
+        videos[1].Comments.Add(new Comment("User 3", "Comment 1 for Second Video "));
+        videos[2].Comments.Add(new Comment("User 4", "Comment 1 for Thrid Video "));
+        videos[2].Comments.Add(new Comment("User 5", "Comment 2 for Third Video "));
+
+        // Display videos and comments
+        foreach (var video in videos)
+        {
+            Console.WriteLine("Title: {0}", video.Title);
+            Console.WriteLine("Author: {0}", video.Author);
+            Console.WriteLine("Length: {0} seconds", video.Length);
+            Console.WriteLine("Number of comments: {0}", video.GetNumberOfComments());
+            foreach (var comment in video.Comments)
+            {
+                Console.WriteLine("Comment by {0}: {1}", comment.CommenterName, comment.Text);
+            }
+            Console.WriteLine();
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,172 @@
 using System;
+using System.Collections.Generic;
+
+class Product
+{
+    private string name;
+    private string productId;
+    private double pricePerUnit;
+    private int quantity;
+
+    public Product(string name, string productId, double pricePerUnit, int quantity)
+    {
+        this.name = name;
+        this.productId = productId;
+        this.pricePerUnit = pricePerUnit;
+        this.quantity = quantity;
+    }
+
+    public double GetTotalCost()
+    {
+        return pricePerUnit * quantity;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public string GetProductId()
+    {
+        return productId;
+    }
+
+    public int GetQuantity()
+    {
+        return quantity;
+    }
+}
+
+class Address
+{
+    private string streetAddress;
+    private string city;
+    private string stateProvince;
+    private string country;
+
+    public Address(string streetAddress, string city, string stateProvince, string country)
+    {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.stateProvince = stateProvince;
+        this.country = country;
+    }
+
+    public bool IsInUSA()
+    {
+        return country == "USA";
+    }
+
+    public string GetAddressDetails()
+    {
+        return $"{streetAddress}\n{city}, {stateProvince}\n{country}";
+    }
+}
+
+class Customer
+{
+    private string name;
+    private Address address;
+
+    public Customer(string name, Address address)
+    {
+        this.name = name;
+        this.address = address;
+    }
+
+    public bool IsInUSA()
+    {
+        return address.IsInUSA();
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public Address GetAddress()
+    {
+        return address;
+    }
+}
+
+class Order
+{
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(Customer customer)
+    {
+        this.customer = customer;
+        products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public double ComputeTotalPrice()
+    {
+        double totalCost = 0;
+        foreach (var product in products)
+        {
+            totalCost += product.GetTotalCost();
+        }
+        totalCost += customer.IsInUSA() ? 5 : 35; // Shipping cost
+        return totalCost;
+    }
+
+    public string CreatePackingLabel()
+    {
+        string label = "";
+        foreach (var product in products)
+        {
+            label += $"{product.GetName()} ({product.GetProductId()})\n";
+        }
+        return label;
+    }
+
+    public string CreateShippingLabel()
+    {
+        return $"{customer.GetName()}\n{customer.GetAddress().GetAddressDetails()}";
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Address address1 = new Address("42 st 1 West", "Rexburg", "ID", "USA");
+        Customer customer1 = new Customer("John Doe", address1);
+
+        Address address2 = new Address("1999 north west temple street", "Salt Lake City", "UT", "USA");
+        Customer customer2 = new Customer("Jane Smith", address2);
+
+        Product product1 = new Product("Widget", "W123", 10.99, 2);
+        Product product2 = new Product("Gadget", "G456", 19.99, 1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product2);
+
+        Console.WriteLine("Order 1:");
+        Console.WriteLine("Packing Label:");
+        Console.WriteLine(order1.CreatePackingLabel());
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order1.CreateShippingLabel());
+        Console.WriteLine("Total Price: $" + order1.ComputeTotalPrice());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Order 2:");
+        Console.WriteLine("Packing Label:");
+        Console.WriteLine(order2.CreatePackingLabel());
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order2.CreateShippingLabel());
+        Console.WriteLine("Total Price: $" + order2.ComputeTotalPrice());
     }
 }
